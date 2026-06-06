3e325e35b5b85d47b411f93e1d77df7101169c44
DINNER TECH PERÚ <dinnertechperu21@gmail.com>
2026-06-06 14:44:30 -0500

    sdsds

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 import joblib
-import pandas as pd
 import os
+import pandas as pd
 
 app = FastAPI()
 
-# cargar modelo
 BASE_DIR = os.path.dirname(__file__)
 modelo = joblib.load(os.path.join(BASE_DIR, "modelo_asistencias.pkl"))
 
-class InputData(BaseModel):
-    alumno_id: int
-    estado: int
+@app.get("/")
+def home():
+    return {"status": "API funcionando"}
 
 @app.post("/predict")
-def predict(data: InputData):
+def predict(data: dict):
+
+    alumno_id = data["alumno_id"]
+    estado = data["estado"]
 
     nuevo = pd.DataFrame([{
-        "alumno_id": data.alumno_id,
-        "estado_num": data.estado
+        "alumno_id": alumno_id,
+        "estado_num": estado
     }])
 
     pred = modelo.predict(nuevo)[0]
     prob = modelo.predict_proba(nuevo)[0]
 
-    estados = {0: "PRESENTE", 1: "TARDE", 2: "FALTA"}
-
     return {
-        "prediccion": estados[int(pred)],
-        "presente": round(prob[0] * 100, 2),
-        "tarde": round(prob[1] * 100, 2),
-        "falta": round(prob[2] * 100, 2)
+        "prediccion": int(pred),
+        "presente": float(prob[0]),
+        "tarde": float(prob[1]),
+        "falta": float(prob[2])
     }
