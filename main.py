cdec66b54a1c1f328a046e5a77eed29739d533da
DINNER TECH PERÚ <dinnertechperu21@gmail.com>
2026-07-21 19:04:41 -0500

    Add predict_multiple endpoint for multiple predictions
    Added a new endpoint to predict multiple student attendance based on input data. The predictions include the student's status and probabilities for being present, late, or absent.

--- main.py
+++ main.py
@@ -27,8 +27,44 @@ def predict(data: dict):
     prob = modelo.predict_proba(nuevo)[0]
 
     return {
+        "alumno_id": alumno_id,
         "prediccion": int(pred),
+        "presente": round(float(prob[0])*100,2),
+        "tarde": round(float(prob[1])*100,2),
+        "falta": round(float(prob[2])*100,2)
+    }
         "presente": float(prob[0]),
         "tarde": float(prob[1]),
         "falta": float(prob[2])
+    }
+
+@app.post("/predict-multiple")
+def predict_multiple(data: dict):
+    alumnos = data["alumnos"]
+    df = pd.DataFrame([
+        {
+            "alumno_id": alumno["alumno_id"],
+            "estado_num": alumno["estado"]
+        }
+        for alumno in alumnos
+    ])
+    pred = modelo.predict(df)
+    prob = modelo.predict_proba(df)
+    resultados=[]
+    estados = {
+        0:"PRESENTE",
+        1:"TARDE",
+        2:"FALTA"
+    }
+    for i, alumno in enumerate(alumnos):
+        resultados.append({
+            "alumno_id": alumno["alumno_id"],
+            "prediccion": estados[int(pred[i])],
+            "presente": round(float(prob[i][0])*100,2),
+            "tarde": round(float(prob[i][1])*100,2),
+            "falta": round(float(prob[i][2])*100,2)
+        })
+    return {
+        "total":len(resultados),
+        "resultados":resultados
     }
